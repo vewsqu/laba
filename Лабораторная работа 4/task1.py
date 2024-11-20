d4cb22c4c1fa91fd252dacfb4225c4afadaa4e8b
vewsqu <vklementieva220505@gmail.com>
2024-11-20 21:42:30 +0300

    Update task1.py

--- Лабораторная работа 4/task1.py
+++ Лабораторная работа 4/task1.py
@@ -3,16 +3,10 @@
 import json
 
 def task() -> float:
-    data = [
-        {"score": 0.0009456152645028281, "weight": 1},
-        {"score": 0.00020640167757499364, "weight": 1},
-        {"score": 0, "weight": 1},
-        {"score": 1.6557065217391307, "weight": 1},
-        {"score": 0, "weight": 1},
-        {"score": 0.6066065217391303, "weight": 1},
-        {"score": 0.03126181644071977, "weight": 1},
-        {"score": 0.001253973281817707, "weight": 1}
-    ]
+
+    with open('input.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
 
     total_sum = 0.0
 
@@ -26,4 +20,3 @@ def task() -> float:
 
 result = task()
 print(result)
-
